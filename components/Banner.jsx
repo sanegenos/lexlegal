ddd1f3d6fff19afe950af30e76beeef6bcc23893
Niko <izhaevm@gmail.com>
2024-08-14 16:20:41 +0300

    14.08

--- components/Banner.jsx
+++ components/Banner.jsx
@@ -3,21 +3,21 @@ import LeadForm from "./LeadForm";
 // components/Banner.jsx
 export default function Banner() {
     return (
-        <section className="flex flex-col md:flex-row justify-between items-center bg-cover bg-center bg-no-repeat relative min-h-screen" style={{backgroundImage: "url('/img/banner.jpg')"}}>
-            <div className="absolute inset-0 bg-black opacity-50"></div>
+        <section className="flex flex-col md:flex-row justify-between items-center bg-cover bg-center bg-no-repeat relative min-h-screen" style={{backgroundImage: "url('/img/Spouse-Visa.jpg')"}}>
+            {/* <div className="absolute inset-0 bg-black opacity-50"></div> */}
             <div className="relative z-10 flex flex-col md:flex-row justify-between items-center w-full max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
                 <div className="md:w-1/2 mb-8 md:mb-0">
-                    <h2 className="text-sm text-purple-700 font-semibold mb-2">WE ARE</h2>
-                    <h1 className="text-4xl font-bold text-navy-900 mb-4">
+                    <h2 className="text-xl text-[#000080] font-semibold mb-2">WE ARE</h2>
+                    <h1 className="text-4xl text-black font-bold mb-4">
                         Reliable &<br/>
                         Experienced UK<br/>
                         Immigration<br/>
                         Lawyers
                     </h1>
-                    <p className="text-white mb-4">
+                    <p className="mb-4 text-black">
                     We are a team of experienced immigration lawyers that care about your future.
                     </p>
-                    <p className="text-white mb-6">
+                    <p className="mb-6 text-black">
                     Call us on <span className="text-yellow-500">+44 77 3969 9968</span> for immediate help and assistance with your situation. We're here to help you in person, via the phone or online.
                     </p>
                     <button className="bg-yellow-400 text-navy-900 font-semibold py-2 px-6 rounded-full hover:bg-yellow-500 transition duration-300">
--- src/app/page.js
+++ src/app/page.js
@@ -18,17 +18,18 @@ export default function Home() {
       <Header/>
       <Banner/>
       <ParallaxProvider>
+      <ParallaxSection 
+          imageUrl="/img/Birlesik-Krallik-Sponsor-Lisansi-Basvurularinda-Gercek-Sirketin-Tanimi-ve-Kriterleri-scaled.jpg" 
+          title="Our Commitment to Excellence" 
+          description="Providing top-notch legal services for over two decades" 
+        />
         <HeroWithVideoBackground 
           videoSrc="/videoplayback.mp4" 
           title="Your Legal Partner for Success" 
           description="Experience unparalleled legal expertise" 
         />
-        <Timeline />
-        <ParallaxSection 
-          imageUrl="/img/Birlesik-Krallik-Sponsor-Lisansi-Basvurularinda-Gercek-Sirketin-Tanimi-ve-Kriterleri-scaled.jpg" 
-          title="Our Commitment to Excellence" 
-          description="Providing top-notch legal services for over two decades" 
-        />
+        {/* <Timeline /> */}
+        
         <Features id="features" />
         <Testimonials id="testimonials" />
         <ParallaxSection 
@@ -42,11 +43,60 @@ export default function Home() {
           title="Client-Centered Approach" 
           description="We prioritize your needs and interests" 
         />
-        <footer id="contact" className="bg-white text-black py-10 text-center">
-          <p className="text-sm">
-            &copy; {new Date().getFullYear()} Lex Legal. All Rights Reserved.
-          </p>
+            
+        <footer class="bg-gray-900 text-white py-10 flex flex-col">
+
+          <div class="container mx-auto flex flex-col md:flex-row justify-between items-center max-w-7xl border-b border-gray-700 pb-10">
+              <div class="mb-8 md:mb-0 text-center md:text-left">
+                  <div class="flex items-center justify-center md:justify-start mb-4">
+                      {/* <img src="logo.png" alt="Logo" class="h-10 w-25 mr-2"/> */}
+                      <span class="text-2xl font-bold">LexLegal</span>
+                  </div>
+              </div>
+
+              <div class="mb-8 md:mb-0">
+                  <ul class="flex space-x-8 text-sm justify-center">
+                      <li><a href="#" class="hover:text-gray-400">Our Lawyers</a></li>
+                      <li><a href="#" class="hover:text-gray-400">Practice Area</a></li>
+                      <li><a href="#" class="hover:text-gray-400">Blog</a></li>
+                      <li><a href="#" class="hover:text-gray-400">Contact</a></li>
+                  </ul>
+              </div>
+          </div>
+          <div class="container mx-auto flex flex-col md:flex-row justify-between items-center max-w-7xl py-10">
+
+              <div class="mb-8 md:mb-0 text-center md:text-left">
+                  <p class="text-sm">
+                      ADDRESS: Marylebone, London W1H 5PW | Meridien House, 42 Upper Berkeley S
+                  </p>
+                  <p class="text-sm">
+                      <span class="font-semibold">Monday - Friday:</span> 10:00 - 6:00 PM
+                  </p>
+              </div>
+              
+              <div class="container mx-auto text-center flex flex-col items-center">
+                  <div class="mt-4 flex justify-center md:justify-end space-x-4">
+                      <a href="#" class="text-white"><i class="fab fa-instagram"></i></a>
+                      <a href="#" class="text-white"><i class="fab fa-linkedin"></i></a>
+                      <a href="#" class="text-white"><i class="fab fa-youtube"></i></a>
+                  </div>
+                  <div class="text-sm ">
+                      © {new Date().getFullYear()} Lex Legal. All Rights Reserved.
+                  </div>
+              </div>
+                  
+              <div class="text-center md:text-right">
+                  <p class="text-sm">
+                      <a href="mailto:info@lexlegal.com" class="hover:text-gray-400">info@lexlegal.com</a>
+                  </p>
+                  <p class="text-sm">
+                      <a href="tel:+447739699968" class="hover:text-gray-400">+44 77 3969 9968</a>
+                  </p>
+              </div>
+          </div>
+
         </footer>
+
       </ParallaxProvider>
     </>
   );
