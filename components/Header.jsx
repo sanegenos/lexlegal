6cceaf371a5d2bf0568bb6ec5df90b4a30c88fe9
Niko <izhaevm@gmail.com>
2024-08-28 11:26:22 +0300

    28.08 - logos

--- components/Header.jsx
+++ components/Header.jsx
@@ -6,14 +6,14 @@ export default function Header() {
         <header id="header" className="bg-white py-2 flex flex-col justify-center items-center w-full border-b relative z-20">
             <div className="container mx-auto flex flex-col md:flex-row justify-between items-center border-b">
                 <div className="flex space-x-4 mb-4 md:mb-0">
-                    <a href="https://www.instagram.com/lexlegal_ltd/" aria-label="Instagram" className="text-gray-600 hover:text-gray-900"><FaInstagram size={30} style={{ color: '#C13584' }} /></a>
-                    <a href="https://tr.linkedin.com/company/lexlegal-ltd" aria-label="LinkedIn" className="text-gray-600 hover:text-gray-900"><FaLinkedin size={30} style={{ color: '#0A66C2' }} /></a>
-                    <a href="https://www.youtube.com/@LexLegalLtd" aria-label="YouTube" className="text-gray-600 hover:text-gray-900"><FaYoutube size={30} style={{ color: '#FF0000' }} /></a>
-                    <a href="https://twitter.com/lexlegal_uk" aria-label="Twitter" className="text-gray-600 hover:text-gray-900"><FaTwitter size={30} style={{ color: '#00acee' }} /></a>
-                    <a href="https://www.facebook.com/lexlegaluk" aria-label="Facebook" className="text-gray-600 hover:text-gray-900"><FaFacebook size={30} style={{ color: '#3b5998' }} /></a>
-                    <a href="mailto:contact@lexlegal.com" aria-label="Facebook" className="text-gray-600 hover:text-gray-900"><FaEnvelope size={30} style={{ color: '#0A66C2' }} /></a>
-                    <a href="https://maps.app.goo.gl/k4jSaiAuCUpUqcaz6" aria-label="Facebook" className="text-gray-600 hover:text-gray-900"><FaMap size={30} style={{ color: '#3b5998' }} /></a>
-                    <a href="tel:+442039880575" aria-label="Facebook" className="text-gray-600 hover:text-gray-900"><FaPhone size={30} style={{ color: '#0A66C2' }} /></a>
+                    <a href="https://www.instagram.com/lexlegal_ltd/" aria-label="Instagram" className="text-gray-600 hover:text-gray-900"><FaInstagram size={20} style={{ color: '#C13584' }} /></a>
+                    <a href="https://tr.linkedin.com/company/lexlegal-ltd" aria-label="LinkedIn" className="text-gray-600 hover:text-gray-900"><FaLinkedin size={20} style={{ color: '#0A66C2' }} /></a>
+                    <a href="https://www.youtube.com/@LexLegalLtd" aria-label="YouTube" className="text-gray-600 hover:text-gray-900"><FaYoutube size={20} style={{ color: '#FF0000' }} /></a>
+                    <a href="https://twitter.com/lexlegal_uk" aria-label="Twitter" className="text-gray-600 hover:text-gray-900"><FaTwitter size={20} style={{ color: '#00acee' }} /></a>
+                    <a href="https://www.facebook.com/lexlegaluk" aria-label="Facebook" className="text-gray-600 hover:text-gray-900"><FaFacebook size={20} style={{ color: '#3b5998' }} /></a>
+                    <a href="mailto:contact@lexlegal.com" aria-label="Facebook" className="text-gray-600 hover:text-gray-900"><FaEnvelope size={20} style={{ color: '#0A66C2' }} /></a>
+                    <a href="https://maps.app.goo.gl/k4jSaiAuCUpUqcaz6" aria-label="Facebook" className="text-gray-600 hover:text-gray-900"><FaMap size={20} style={{ color: '#3b5998' }} /></a>
+                    <a href="tel:+442039880575" aria-label="Facebook" className="text-gray-600 hover:text-gray-900"><FaPhone size={20} style={{ color: '#0A66C2' }} /></a>
                 </div>
 
                 <div className="flex items-center mb-4 md:mb-0 max-w-72">
